aff9f6e3891e373be5c6b515cf14f33dcaed2a55
Uruanth <82775623+Uruanth@users.noreply.github.com>
2021-11-20 09:06:36 -0500

    Correccion back

--- Back/back_Alternativo/hackathon/src/main/java/edu/unelbosque/hackathon/Repository/PorveedorRepository.java
+++ Back/back_Alternativo/hackathon/src/main/java/edu/unelbosque/hackathon/Repository/PorveedorRepository.java
@@ -1,19 +1,18 @@
 package edu.unelbosque.hackathon.Repository;
 
 import edu.unelbosque.hackathon.Models.Proveedor;
-import edu.unelbosque.hackathon.Models.Proveedor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
 public interface PorveedorRepository extends MongoRepository<Proveedor, String> {
-	
-	/**
-	 * @param nombre
-	 * @return list of suppliers found by name
-	 */
+
+    /**
+     * @param nombre
+     * @return list of suppliers found by name
+     */
     List<Proveedor> findByNombre(String nombre);
-    
+
     /**
      * @param codigo
      * @return list of suppliers found by code
