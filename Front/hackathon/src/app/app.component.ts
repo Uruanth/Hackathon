2108ec815ce4548abad01a60b9f14d2f7fbf6e68
gandalfblue <casanova01_a@hotmail.com>
2021-11-28 18:25:54 -0500

    Cambios de estilos en el front

--- Front/hackathon/src/app/app.component.ts
+++ Front/hackathon/src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hackathon';
+
+  mostrar: boolean = false;
+
+  public sidebarColor: string = "red";
+
+  constructor() {}
+  changeSidebarColor(color:any){
+    var sidebar = document.getElementsByClassName('sidebar')[0];
+    var mainPanel = document.getElementsByClassName('main-panel')[0];
+
+    this.sidebarColor = color;
+
+    if(sidebar != undefined){
+        sidebar.setAttribute('data',color);
+    }
+    if(mainPanel != undefined){
+        mainPanel.setAttribute('data',color);
+    }
+  }
+
+  changeDashboardColor(color:any){
+    var body = document.getElementsByTagName('body')[0];
+    if (body && color === 'white-content') {
+        body.classList.add(color);
+    }
+    else if(body.classList.contains('white-content')) {
+      body.classList.remove('white-content');
+    }
+  }
+
+  funcion_mostrar() {  
+    this.mostrar = !this.mostrar ;
+  }
 }
